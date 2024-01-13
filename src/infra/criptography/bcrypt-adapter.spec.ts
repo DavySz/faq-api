d7823b7bc83a89c4104172936c5551a3fd868528
davy <davy.assuncao-loldesign@fretebras.com.br>
2024-01-13 14:23:12 -0400

    test: ensure BcryptAdapter return hashed value on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,5 +1,12 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
+
+jest.mock('bcrypt', () => ({
+  hash: async (): Promise<string> => {
+    return await Promise.resolve('hashed-value')
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   it('should calls Bcrypt with correct values', async () => {
     const sut = new BcryptAdapter()
@@ -9,5 +16,11 @@ describe('Bcrypt Adapter', () => {
 
     await sut.encrypt('any-value')
     expect(hashSpy).toHaveBeenCalledWith('any-value', salt)
+  })
+
+  it('should return an hashed value on success', async () => {
+    const sut = new BcryptAdapter()
+    const hash = await sut.encrypt('any-value')
+    expect(hash).toBe('hashed-value')
   })
 })
