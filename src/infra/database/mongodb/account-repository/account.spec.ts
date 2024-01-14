ef6c41aa603f4506607f34ffaa757bf9cab5115b
davy <davy.assuncao-loldesign@fretebras.com.br>
2024-01-14 14:27:30 -0400

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/database/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(String(process.env.MONGO_URL))
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'valid-name',
+      password: 'valid-password',
+      email: 'any-mail@mail.com'
+    })
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('valid-name')
+    expect(account.email).toBe('any-mail@mail.com')
+    expect(account.password).toBe('valid-password')
+  })
+})
