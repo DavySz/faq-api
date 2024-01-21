71190458850af412cac97b2fed31a360b6feaf5e
davy <davy.assuncao-loldesign@fretebras.com.br>
2024-01-21 12:16:28 -0400

    feat: add express route adapter

--- src/main/routes/sign-up-routes.ts
+++ src/main/routes/sign-up-routes.ts
@@ -1,7 +1,7 @@
-import { type Router } from 'express'
+import type { Router } from 'express'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeSignUpController } from '../factories/sign-up'
 
 export default (router: Router): void => {
-  router.post('/sign-up', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  router.post('/sign-up', adaptRoute(makeSignUpController()))
 }
