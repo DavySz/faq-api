8ea2f09952094b2100bf4d9f4c80b7893904441f
davy <davy.assuncao-loldesign@fretebras.com.br>
2024-01-21 20:33:55 -0400

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,31 +1,49 @@
 import type { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+interface IMakeSut {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    httpRequest: HttpRequest
+
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      this.httpRequest = httpRequest
+
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'any-name',
+          email: 'email@mail.com',
+          password: 'any-password'
+        }
+      }
+
+      return await Promise.resolve(httpResponse)
+    }
+  }
+
+  return new ControllerStub()
+}
+
+const makeSut = (): IMakeSut => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    controllerStub,
+    sut
+  }
+}
+
 describe('Log Controller Decorator', () => {
   it('should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      httpRequest: HttpRequest
-
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        this.httpRequest = httpRequest
-
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'any-name',
-            email: 'email@mail.com',
-            password: 'any-password'
-          }
-        }
-
-        return await Promise.resolve(httpResponse)
-      }
-    }
-
-    const controllerStub = new ControllerStub()
+    const { controllerStub, sut } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
 
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest: HttpRequest = {
       body: {
         name: 'any-name',
