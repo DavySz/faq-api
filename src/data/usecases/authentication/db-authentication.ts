68c11d182a0f055a9539e788a86e5f4d41654798
Davy Assunção <davydesouzabar@gmail.com>
2024-01-30 14:36:41 -0400

    feat: ensure DbAuthentication call UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,14 +1,16 @@
 import type { AuthenticationModel, Authentication } from '../../../domain/usecases/authentication'
 import type { Nullable } from '../../../presentation/common/types'
 import type { HashCompare } from '../../protocols/criptography/hash-compare'
-import { type TokenGenerator } from '../../protocols/criptography/token-generator'
+import type { TokenGenerator } from '../../protocols/criptography/token-generator'
 import type { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
+import type { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository'
 
 export class DbAuthenticationRepository implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
     private readonly hashCompare: HashCompare,
-    private readonly tokenGenerator: TokenGenerator
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository
   ) {}
 
   async auth (authentication: AuthenticationModel): Promise<Nullable<string>> {
@@ -26,6 +28,8 @@ export class DbAuthenticationRepository implements Authentication {
     }
 
     const accessToken = await this.tokenGenerator.generate(account.id)
+
+    await this.updateAccessTokenRepository.update(account.id, accessToken)
 
     return accessToken
   };
