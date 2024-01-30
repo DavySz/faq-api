8db810c51151fb29d04a2954d0fe9b5dfc06b10c
Davy Assunção <davydesouzabar@gmail.com>
2024-01-30 13:58:09 -0400

    refactor: move data layer interfaces to folders

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,4 +1,4 @@
-import { type LogErrorRepository } from '../../data/protocols/log-error-repository'
+import { type LogErrorRepository } from '../../data/protocols/db/log-error-repository'
 import type { HttpRequest, Controller, HttpResponse } from '../../presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
